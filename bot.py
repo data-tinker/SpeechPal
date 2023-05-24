89e2f37cf026a566a33efb6590ee0f9f199454a8
ttymonkey <asmartishin@gmail.com>
2023-05-24 12:52:56 +0200

    Add welcome message

--- bot.py
+++ bot.py
@@ -20,6 +20,24 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 bot = telebot.TeleBot(os.getenv('BOT_TOKEN'), num_threads=10)
 
 
+@bot.message_handler(commands=['start'], content_types=['text'])
+def process_youtube_video(message):
+    bot.send_message(
+        message.from_user.id,
+        """
+Enjoy using the SpeechPal Telegram bot to analyze your speech in audio and video formats, and improve your speaking skills with personalized recommendations.
+
+To analyze your speech, you have multiple options:
+
+🔴Audio message: simply upload it to the bot by sending the file or recording a new voice message within the chat.
+🔴Audio on your phone: send the file to the bot by tapping on the paperclip icon in the chat and selecting the file.
+🔴YouTube video: type /youtube and the video's link right afterward.
+
+After you've provided the audio or video to the bot, it will process the content and analyze your speech. Once the analysis is complete, the bot will send you a detailed message with recommendations to help you improve your speech.
+        """
+    )
+
+
 @bot.message_handler(commands=['youtube'], content_types=['text'])
 def process_youtube_video(message):
     youtube_link = message.text.split()[1]
